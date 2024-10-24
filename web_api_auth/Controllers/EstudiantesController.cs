ba667e631422f36584046c15eb567a647bbc7d94
juanlopexgold <juanlopexdhegold@gmail.com>
2024-10-23 18:43:14 -0600

    configuracion_inicial

---
+++ web_api_auth/Controllers/EstudiantesController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace web_api_auth.Controllers
+{
+    [Route("[controller]")]
+    public class EstudiantesController : Controller
+    {
+        private readonly ILogger<EstudiantesController> _logger;
+
+        public EstudiantesController(ILogger<EstudiantesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
